526b45f5cf41b9d04674a951cfc26fa5b155568e
hesamriahinia <hesamriahinia@gmail.com>
2024-04-17 18:45:15 +0330

    update database in model

--- hesam/admin.py
+++ hesam/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ('name', 'email', 'created_date')
+    list_filter = ('email',)
+    search_fields = ('name', 'message')
+
+
+admin.site.register(Contact,ContactAdmin)
